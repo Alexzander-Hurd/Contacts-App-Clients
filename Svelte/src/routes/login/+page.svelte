a08291178e70c2448df55e29b8a035221aca0b65
Alex Hurd <ajghurd@gmail.com>
2026-02-04 12:53:22 +0000

    matched login focus borders to other text boxes

--- Svelte/src/routes/login/+page.svelte
+++ Svelte/src/routes/login/+page.svelte
@@ -74,7 +74,7 @@
 						for="username">Email</label
 					>
 					<input
-						class="border-plum-border focus:ring-primary h-14 w-full rounded-xl border bg-[#faf5ff] dark:bg-[#2d1b36] px-4 text-black dark:text-slate-300 transition-all outline-none placeholder:text-[#715d7a] focus:border-transparent focus:ring-2"
+						class="border-plum-border focus:ring-purple-500 h-14 w-full rounded-xl border bg-[#faf5ff] dark:bg-[#2d1b36] px-4 text-black dark:text-slate-300 transition-all outline-none placeholder:text-[#715d7a] focus:border-transparent focus:ring-2"
 						placeholder="name@example.com"
 						id="username"
 						bind:value={username}
@@ -87,7 +87,7 @@
 					>
 					<div class="relative">
 						<input
-							class="border-plum-border focus:ring-primary h-14 w-full rounded-xl border bg-[#faf5ff] dark:bg-[#2d1b36] px-4 text-black dark:text-slate-300 transition-all outline-none placeholder:text-[#715d7a] focus:border-transparent focus:ring-2"
+							class="border-plum-border focus:ring-purple-500 h-14 w-full rounded-xl border bg-[#faf5ff] dark:bg-[#2d1b36] px-4 text-black dark:text-slate-300 transition-all outline-none placeholder:text-[#715d7a] focus:border-transparent focus:ring-2"
 							placeholder="••••••••"
 							id="password"
 							bind:value={password}
